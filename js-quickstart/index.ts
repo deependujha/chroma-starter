9c1ba20b594b19037d1036e0fb98fcf8a7f9a8e1
deependujha <deependujha21@gmail.com>
2023-05-16 15:50:06 +0530

    both python & typescript code working. The vector database is persisting in case of typescript.

---
+++ js-quickstart/index.ts
@@ -0,0 +1,32 @@
+import { ChromaClient } from 'chromadb';
+import { OpenAIEmbeddingFunction } from 'chromadb';
+
+const embedder = new OpenAIEmbeddingFunction({
+	openai_api_key: process.env.OPENAI_API_KEY || '',
+});
+
+const client = new ChromaClient();
+
+const collection = await client.createCollection({
+	name: 'my_collection',
+	metadata: {},
+	embeddingFunction: embedder,
+});
+
+await collection.add({
+	ids: ['id1', 'id2'],
+	embeddings: undefined,
+	metadatas: [{ source: 'my_source' }, { source: 'my_source' }],
+	documents: ['This is a document', 'This is another document'],
+});
+
+const results = await collection.query({
+	query_embeddings: undefined,
+	n_results: 2,
+	where: undefined,
+	query_text: ['This is a query document'],
+});
+
+// print json results
+console.log(`results:`);
+console.log(JSON.stringify(results, null, 2));
